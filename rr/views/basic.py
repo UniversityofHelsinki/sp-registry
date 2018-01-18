707c3f2d93f5b20ea737a7ed7ccbce410251333e
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2018-01-18 13:43:52 +0200

    fix show login page before trying shibboleth auth

--- rr/views/basic.py
+++ rr/views/basic.py
@@ -1,29 +1,22 @@
 from django.shortcuts import render
 from django.views import View
-from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from rr.auth.shibboleth import ShibbolethBackend
 from django.utils.translation import ugettext_lazy as _
 from django.http.response import HttpResponseRedirect
 from django.conf import settings
+from django.urls.base import reverse
 
 
-class FrontPage(View):
-    template_name = 'rr/front_page.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
-
-
-class CustomLoginView(LoginView):
-
+class ShibbolethLoginView(View):
     def get(self, request, *args, **kwargs):
+        redirect_to = request.GET.get('next', '/')
+        print(redirect_to)
         user = ShibbolethBackend.authenticate(self, request)
         if user:
             if user.is_active:
                 login(request, user, backend='rr.auth.shibboleth.ShibbolethBackend')
-                redirect_to = self.get_success_url()
-                if redirect_to == self.request.path:
+                if redirect_to == request.path:
                     error_message = _("Redirection loop for authenticated user detected. Check that your LOGIN_REDIRECT_URL doesn't point to a login page.")
                     return render(request, "error.html", {'error_message': error_message})
                 return HttpResponseRedirect(redirect_to)
@@ -32,4 +25,4 @@ class CustomLoginView(LoginView):
                 contact = settings.DEFAULT_CONTACT_EMAIL
                 return render(request, "info.html", {'info_message': info_message,
                                                      'contact': contact})
-        return super().get(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('serviceprovider-list'))

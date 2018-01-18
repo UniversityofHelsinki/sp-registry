f71b1ba8ac68fe451ae8f2c99d75c4258216be4a
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2018-01-18 13:10:27 +0200

    style colors legend for summary

--- rr/local_settings_example.py
+++ rr/local_settings_example.py
@@ -64,3 +64,7 @@ SAML_ATTR_EMAIL = 'shib_mail'
 SAML_ATTR_AFFILIATION = 'shib_affiliation'
 
 STATIC_ROOT = '/path/to/rr/static/'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True

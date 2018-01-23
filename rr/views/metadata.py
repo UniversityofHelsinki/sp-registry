b9c4f8b626a750404f40b0ed90d54fbf99a54561
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2018-01-23 10:53:49 +0200

    refact split to functions

--- rr/views/metadata.py
+++ rr/views/metadata.py
@@ -12,71 +12,60 @@ from django.contrib.auth.decorators import login_required
 from lxml import etree, objectify
 
 
-def metadata_generator(sp):
-    """
-    Using CamelCase instead of regular underscore attribute names in element tree.
-    Generates metadata for single SP.
-    """
-    basicinfo = sp
-    certificates = Certificate.objects.filter(sp=sp, end_at=None)
-    contacts = Contact.objects.filter(sp=sp, end_at=None)
-    endpoints = Endpoint.objects.filter(sp=sp, end_at=None)
-    attributes = SPAttribute.objects.filter(sp=sp)
-
-    EntityDescriptor = etree.Element("EntityDescriptor",
-                                     schemaLocation="urn:oasis:names:tc:SAML:2.0:metadata",
-                                     entityID=basicinfo.entity_id)
-    SPSSODescriptor = etree.SubElement(EntityDescriptor, "SPSSODescriptor", protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol")
-    Extensions = etree.SubElement(SPSSODescriptor, "Extensions")
-    if basicinfo.discovery_service_url:
-        DiscoveryResponse = etree.SubElement(Extensions, "{urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol}DiscoveryResponse",
-                                             Binding="urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol",
-                                             Location=basicinfo.discovery_service_url,
-                                             index="1",
-                                             nsmap={"idpdisc": 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol'})
+def metadata_extensions(element, sp):
+    Extensions = etree.SubElement(element, "Extensions")
+    if sp.discovery_service_url:
+        etree.SubElement(Extensions, "{urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol}DiscoveryResponse",
+                         Binding="urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol",
+                         Location=sp.discovery_service_url,
+                         index="1",
+                         nsmap={"idpdisc": 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol'})
     UIInfo = etree.SubElement(Extensions, "{urn:oasis:names:tc:SAML:metadata:ui}UIInfo",
-                               nsmap={"mdui": 'urn:oasis:names:tc:SAML:metadata:ui'})
-    if basicinfo.name_fi:
+                              nsmap={"mdui": 'urn:oasis:names:tc:SAML:metadata:ui'})
+    if sp.name_fi:
         DisplayName_fi = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}DisplayName")
         DisplayName_fi.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "fi"
-        DisplayName_fi.text = basicinfo.name_fi
-    if basicinfo.name_en:
+        DisplayName_fi.text = sp.name_fi
+    if sp.name_en:
         DisplayName_en = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}DisplayName")
         DisplayName_en.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
-        DisplayName_en.text = basicinfo.name_en
-    if basicinfo.name_sv:
+        DisplayName_en.text = sp.name_en
+    if sp.name_sv:
         DisplayName_sv = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}DisplayName")
         DisplayName_sv.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "sv"
-        DisplayName_sv.text = basicinfo.name_sv
+        DisplayName_sv.text = sp.name_sv
 
-    if basicinfo.description_fi:
+    if sp.description_fi:
         Description_fi = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}Description")
         Description_fi.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "fi"
-        Description_fi.text = basicinfo.description_fi
-    if basicinfo.description_en:
+        Description_fi.text = sp.description_fi
+    if sp.description_en:
         Description_en = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}Description")
         Description_en.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
-        Description_en.text = basicinfo.description_en
-    if basicinfo.description_sv:
+        Description_en.text = sp.description_en
+    if sp.description_sv:
         Description_sv = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}Description")
         Description_sv.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "sv"
-        Description_sv.text = basicinfo.description_sv
+        Description_sv.text = sp.description_sv
 
-    if basicinfo.privacypolicy_fi:
+    if sp.privacypolicy_fi:
         PrivacyStatementURL_fi = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}PrivacyStatementURL")
         PrivacyStatementURL_fi.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "fi"
-        PrivacyStatementURL_fi.text = basicinfo.privacypolicy_fi
-    if basicinfo.privacypolicy_en:
+        PrivacyStatementURL_fi.text = sp.privacypolicy_fi
+    if sp.privacypolicy_en:
         PrivacyStatementURL_en = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}PrivacyStatementURL")
         PrivacyStatementURL_en.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
-        PrivacyStatementURL_en.text = basicinfo.privacypolicy_en
-    if basicinfo.privacypolicy_sv:
+        PrivacyStatementURL_en.text = sp.privacypolicy_en
+    if sp.privacypolicy_sv:
         PrivacyStatementURL_sv = etree.SubElement(UIInfo, "{urn:oasis:names:tc:SAML:metadata:ui}PrivacyStatementURL")
         PrivacyStatementURL_sv.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "sv"
-        PrivacyStatementURL_sv.text = basicinfo.privacypolicy_sv
+        PrivacyStatementURL_sv.text = sp.privacypolicy_sv
 
+
+def metadata_certificates(element, sp):
+    certificates = Certificate.objects.filter(sp=sp, end_at=None)
     for certificate in certificates:
-        KeyDescriptor = etree.SubElement(SPSSODescriptor, "KeyDescriptor",
+        KeyDescriptor = etree.SubElement(element, "KeyDescriptor",
                                          nsmap={"ds": 'urn:oasis:names:tc:SAML:2.0:metadata'})
         if certificate.signing and not certificate.encryption:
             KeyDescriptor.attrib['use'] = 'signing'
@@ -89,56 +78,89 @@ def metadata_generator(sp):
         X509Certificate = etree.SubElement(X509Data, "{urn:oasis:names:tc:SAML:2.0:metadata}X509Certificate")
         X509Certificate.text = certificate.certificate
 
-    if basicinfo.name_format_transient:
-        NameIDFormat = etree.SubElement(SPSSODescriptor, "NameIDFormat")
+
+def metadata_nameidformat(element, sp):
+    if sp.name_format_transient:
+        NameIDFormat = etree.SubElement(element, "NameIDFormat")
         NameIDFormat.text = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
-    if basicinfo.name_format_persistent:
-        NameIDFormat = etree.SubElement(SPSSODescriptor, "NameIDFormat")
+    if sp.name_format_persistent:
+        NameIDFormat = etree.SubElement(element, "NameIDFormat")
         NameIDFormat.text = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"
 
-    for endpoint in endpoints:
-        etree.SubElement(SPSSODescriptor, endpoint.type, Binding=endpoint.binding, Location=endpoint.url)
 
-    AttributeConsumingService = etree.SubElement(SPSSODescriptor, "AttributeConsumingService", index="1")
-    if basicinfo.name_fi:
+def metadata_endpoints(element, sp):
+    endpoints = Endpoint.objects.filter(sp=sp, end_at=None)
+    for endpoint in endpoints:
+        etree.SubElement(element, endpoint.type, Binding=endpoint.binding, Location=endpoint.url)
+
+
+def metadata_attributeconsumingservice(element, sp):
+    attributes = SPAttribute.objects.filter(sp=sp)
+    AttributeConsumingService = etree.SubElement(element, "AttributeConsumingService", index="1")
+    if sp.name_fi:
         DisplayName_fi = etree.SubElement(AttributeConsumingService, "ServiceName")
         DisplayName_fi.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "fi"
-        DisplayName_fi.text = basicinfo.name_fi
-    if basicinfo.name_en:
+        DisplayName_fi.text = sp.name_fi
+    if sp.name_en:
         DisplayName_en = etree.SubElement(AttributeConsumingService, "ServiceName")
         DisplayName_en.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
-        DisplayName_en.text = basicinfo.name_en
-    if basicinfo.name_sv:
+        DisplayName_en.text = sp.name_en
+    if sp.name_sv:
         DisplayName_sv = etree.SubElement(AttributeConsumingService, "ServiceName")
         DisplayName_sv.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "sv"
-        DisplayName_sv.text = basicinfo.name_sv
+        DisplayName_sv.text = sp.name_sv
 
-    if basicinfo.description_fi:
+    if sp.description_fi:
         Description_fi = etree.SubElement(AttributeConsumingService, "ServiceDescription")
         Description_fi.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "fi"
-        Description_fi.text = basicinfo.description_fi
-    if basicinfo.description_en:
+        Description_fi.text = sp.description_fi
+    if sp.description_en:
         Description_en = etree.SubElement(AttributeConsumingService, "ServiceDescription")
         Description_en.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
-        Description_en.text = basicinfo.description_en
-    if basicinfo.description_sv:
+        Description_en.text = sp.description_en
+    if sp.description_sv:
         Description_sv = etree.SubElement(AttributeConsumingService, "ServiceDescription")
         Description_sv.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "sv"
-        Description_sv.text = basicinfo.description_sv
+        Description_sv.text = sp.description_sv
 
     for attribute in attributes:
         etree.SubElement(AttributeConsumingService, "RequestedAttribute",
                          FriendlyName=attribute.attribute.friendlyname, Name=attribute.attribute.name,
                          NameFormat=attribute.attribute.nameformat)
 
+
+def metadata_contact(element, sp):
+    contacts = Contact.objects.filter(sp=sp, end_at=None)
     for contact in contacts:
-        ContactPerson = etree.SubElement(EntityDescriptor, "ContactPerson", contactType=contact.type)
+        ContactPerson = etree.SubElement(element, "ContactPerson", contactType=contact.type)
         GivenName = etree.SubElement(ContactPerson, "GivenName")
         GivenName.text = contact.firstname
         SurName = etree.SubElement(ContactPerson, "SurName")
         SurName.text = contact.lastname
         EmailAddress = etree.SubElement(ContactPerson, "EmailAddress")
         EmailAddress.text = contact.email
+
+
+def metadata_spssodescriptor(element, sp):
+    SPSSODescriptor = etree.SubElement(element, "SPSSODescriptor", protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol")
+    metadata_extensions(SPSSODescriptor, sp)
+    metadata_certificates(SPSSODescriptor, sp)
+    metadata_nameidformat(SPSSODescriptor, sp)
+    metadata_endpoints(SPSSODescriptor, sp)
+    metadata_attributeconsumingservice(SPSSODescriptor, sp)
+
+
+def metadata_generator(sp):
+    """
+    Using CamelCase instead of regular underscore attribute names in element tree.
+    Generates metadata for single SP.
+    """
+
+    EntityDescriptor = etree.Element("EntityDescriptor",
+                                     schemaLocation="urn:oasis:names:tc:SAML:2.0:metadata",
+                                     entityID=sp.entity_id)
+    metadata_spssodescriptor(EntityDescriptor, sp)
+    metadata_contact(EntityDescriptor, sp)
 
     return(etree.tostring(EntityDescriptor, pretty_print=True))
 

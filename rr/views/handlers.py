327a814317d1e0cdba72756eae28c8d7b563d180
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2018-07-18 10:04:01 +0300

    fix error handlers for attributetest service

--- rr/views/handlers.py
+++ rr/views/handlers.py
@@ -13,9 +13,20 @@ def bad_request(request):
     return render(request, 'handlers/400.html', values_for_template, status=400)
 
 
+def bad_request_blank(request):
+    logger.warning("Bad request")
+    values_for_template = {}
+    return render(request, 'handlers/400_blank.html', values_for_template, status=400)
+
+
 def permission_denied(request):
     values_for_template = {}
     return render(request, 'handlers/403.html', values_for_template, status=403)
+
+
+def permission_denied_blank(request):
+    values_for_template = {}
+    return render(request, 'handlers/403_blank.html', values_for_template, status=403)
 
 
 def page_not_found(request):
@@ -23,7 +34,18 @@ def page_not_found(request):
     return render(request, 'handlers/404.html', values_for_template, status=404)
 
 
+def page_not_found_blank(request):
+    values_for_template = {}
+    return render(request, 'handlers/404_blank.html', values_for_template, status=404)
+
+
 def server_error(request):
     logger.error("Internal server error")
     values_for_template = {}
     return render(request, 'handlers/500.html', values_for_template, status=500)
+
+
+def server_error_blank(request):
+    logger.error("Internal server error")
+    values_for_template = {}
+    return render(request, 'handlers/500_blank.html', values_for_template, status=500)
--- urls_attributetest.py
+++ urls_attributetest.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from rr.views.attribute_test_service import attribute_test_service
 
 # Overwrite default status handlers
-handler400 = 'rr.views.handlers.bad_request'
-handler403 = 'rr.views.handlers.permission_denied'
-handler404 = 'rr.views.handlers.page_not_found'
-handler500 = 'rr.views.handlers.server_error'
+handler400 = 'rr.views.handlers.bad_request_blank'
+handler403 = 'rr.views.handlers.permission_denied_blank'
+handler404 = 'rr.views.handlers.page_not_found_blank'
+handler500 = 'rr.views.handlers.server_error_blank'
 
 urlpatterns = [
     url(r'^$', attribute_test_service, name='attribute-test-service'),

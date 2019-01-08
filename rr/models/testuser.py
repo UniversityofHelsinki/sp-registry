70ccb23d24f6e86b922dc2127b0b192194a8bcfe
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2019-01-08 10:27:04 +0200

    fix set name fields empty instead of null if no name is given

--- rr/models/testuser.py
+++ rr/models/testuser.py
@@ -12,9 +12,9 @@ class TestUser(models.Model):
     sp = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
     username = models.CharField(max_length=50, verbose_name=_('Login name'))
     password = models.CharField(max_length=64, verbose_name=_('Password'))
-    firstname = models.CharField(blank=True, null=True, max_length=255,
+    firstname = models.CharField(blank=True, max_length=255,
                                  verbose_name=_('First name'))
-    lastname = models.CharField(blank=True, null=True, max_length=255, verbose_name=_('Last name'))
+    lastname = models.CharField(blank=True, max_length=255, verbose_name=_('Last name'))
     valid_for = models.ManyToManyField(ServiceProvider, blank=True, related_name="valid_for",
                                        verbose_name=_('Valid for selected SPs'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))

1c8c11d51f84ad368028a219a7e80a8bdf7ffb98
Jukka Karvonen <jukka.karvonen@helsinki.fi>
2019-09-04 15:09:29 +0300

    Added statistics summary page for superusers

--- rr/views/statistics.py
+++ rr/views/statistics.py
@@ -2,6 +2,8 @@ import logging
 from datetime import date, timedelta
 
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.db.models import Sum
 from django.http.response import Http404
 from django.shortcuts import render
 
@@ -20,7 +22,7 @@ def statistics_list(request, pk):
     **Context**
 
     ``object_list``
-        List of :model:`rr.Endpoint`.
+        List of :model:`rr.Statistics`.
 
     ``object``
         An instance of :model:`rr.ServiceProvider`.
@@ -50,3 +52,38 @@ def statistics_list(request, pk):
     return render(request, "rr/statistics.html", {'object_list': statistics,
                                                   'object': sp,
                                                   'days': days})
+
+
+@login_required
+def statistics_summary_list(request):
+    """
+    Displays a list of :model:`rr.Statistics` linked to
+    :model:`rr.ServiceProvider`.
+
+    **Context**
+
+    ``object_list``
+        List of :model:`rr.Statistics`.
+
+    **Template:**
+
+    :template:`rr/statistics_summary.html`
+    """
+    if not request.user.is_superuser:
+        raise PermissionDenied
+    serviceproviders = ServiceProvider.objects.filter(end_at=None,
+                                                      production=True,
+                                                      service_type="saml").order_by("entity_id")
+    stats = Statistics.objects.all()
+    statistics = []
+    for sp in serviceproviders:
+        weekly = stats.filter(sp=sp, date__gte=(date.today() - timedelta(days=8))
+                              ).aggregate(Sum('logins')).get("logins__sum", 0)
+        monthly = stats.filter(sp=sp, date__gte=(date.today() - timedelta(days=31))
+                               ).aggregate(Sum('logins')).get("logins__sum", 0)
+        six_months = stats.filter(sp=sp, date__gte=(date.today() - timedelta(days=183))
+                              ).aggregate(Sum('logins')).get("logins__sum", 0)
+        yearly = stats.filter(sp=sp, date__gte=(date.today() - timedelta(days=365))
+                              ).aggregate(Sum('logins')).get("logins__sum", 0)
+        statistics.append([sp.pk, sp.entity_id, weekly, monthly, six_months, yearly])
+    return render(request, "rr/statistics_summary.html", {'object_list': statistics})
